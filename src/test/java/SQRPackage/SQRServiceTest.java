2b74bcb342ed8a8f3bddaa20b0ba8988c9fd48c3
EugenyVinogradov <vinogradov_e_v@rambler.ru>
2022-05-06 00:38:45 +0300

    add CsvSourse

--- src/test/java/SQRPackage/SQRServiceTest.java
+++ src/test/java/SQRPackage/SQRServiceTest.java
@@ -2,81 +2,31 @@ package SQRPackage;
 
 import SQRPackage.SQRService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SQRServiceTest {
-    @Test
-    void countSquareSearchWorkValues() {  // Проверка на рабочем диапазоне данных
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "countSquareSearchWorkValues,100,300,8",
+                    "countSquareSearchUnderLowBorder,0,99,0",
+                    "countSquareSearchLowBorder,0,100,1",
+                    "countSquareSearchOverLowBorder,0,101,1",
+                    "countSquareSearchUnderHiBorder,9800,10000,1",
+                    "countSquareSearchHiBorder,9801,10000,1",
+                    "countSquareSearchOverHiBorder,9802,10000,0",
+                    "countSquareSearchNegativeBorder,-10000,0,0"
+            },
+            delimiter = ','
+    )
+
+    void countSquareSearchWorkValues(String test, long minBorder, long maxBorder, long expected) {
         SQRService service = new SQRService();
-        long minBorder = 100;
-        long maxBorder = 300;
-        long expected = 8;
         long actual = service.countSquareSearch(minBorder, maxBorder);
         assertEquals(expected, actual);
     }
 
-    @Test
-    void countSquareSearchUnderLowBorder() {  // Проверка ниже нижней границы данных
-        SQRService service = new SQRService();
-        long minBorder = 0;
-        long maxBorder = 99;
-        long expected = 0;
-        long actual = service.countSquareSearch(minBorder, maxBorder);
-        assertEquals(expected, actual);
-    }
-    @Test
-    void countSquareSearchLowBorder() {  // Проверка нижней границы
-        SQRService service = new SQRService();
-        long minBorder = 0;
-        long maxBorder = 100;
-        long expected = 1;
-        long actual = service.countSquareSearch(minBorder, maxBorder);
-        assertEquals(expected, actual);
-    }
-    @Test
-    void countSquareSearchOverLowBorder() {  // Проверка выше нижней границы
-        SQRService service = new SQRService();
-        long minBorder = 0;
-        long maxBorder = 101;
-        long expected = 1;
-        long actual = service.countSquareSearch(minBorder, maxBorder);
-        assertEquals(expected, actual);
-    }
-    @Test
-    void countSquareSearchUnderHiBorder() {  // Проверка ниже верхней границы
-        SQRService service = new SQRService();
-        long minBorder = 9800;
-        long maxBorder = 10000;
-        long expected = 1;
-        long actual = service.countSquareSearch(minBorder, maxBorder);
-        assertEquals(expected, actual);
-    }
-    @Test
-    void countSquareSearchHiBorder() {  // Проверка верхней границы
-        SQRService service = new SQRService();
-        long minBorder = 9801;
-        long maxBorder = 10000;
-        long expected = 1;
-        long actual = service.countSquareSearch(minBorder, maxBorder);
-        assertEquals(expected, actual);
-    }
-    @Test
-    void countSquareSearchOverHiBorder() {  // Проверка выше верхней границы
-        SQRService service = new SQRService();
-        long minBorder = 9802;
-        long maxBorder = 10000;
-        long expected = 0;
-        long actual = service.countSquareSearch(minBorder, maxBorder);
-        assertEquals(expected, actual);
-    }
-    @Test
-    void countSquareSearchNegativeBorder() {  // Проверка отрицательных границ
-        SQRService service = new SQRService();
-        long minBorder = -10000;
-        long maxBorder = 0;
-        long expected = 0;
-        long actual = service.countSquareSearch(minBorder, maxBorder);
-        assertEquals(expected, actual);
-    }
 }
