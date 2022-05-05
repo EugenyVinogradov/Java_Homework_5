d4dd6b95e4482649850d322fd3eb3724d8a9a8dc
EugenyVinogradov <vinogradov_e_v@rambler.ru>
2022-05-06 00:58:42 +0300

    replacing while with for

--- src/main/java/SQRPackage/SQRService.java
+++ src/main/java/SQRPackage/SQRService.java
@@ -3,15 +3,13 @@ package SQRPackage;
 public class SQRService {
     public long countSquareSearch(long minBorder, long maxBorder) {
         int count = 0;
-        int minValue = 10;
         int maxValue = 99;
-        while (minValue <= maxValue) {
+        for (int minValue = 10; minValue <= maxValue; minValue++) {
             if (minValue * minValue >= minBorder) {
                 if (minValue * minValue <= maxBorder) {
                     count = count + 1;
                 }
             }
-            minValue = minValue + 1;
         }
         return (count);
     }
