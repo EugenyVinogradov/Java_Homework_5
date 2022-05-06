970a22daea009fc0e819f710738ea7db870bb9b3
EugenyVinogradov <vinogradov_e_v@rambler.ru>
2022-05-06 09:54:01 +0300

    replacing the counter variable

--- src/main/java/SQRPackage/SQRService.java
+++ src/main/java/SQRPackage/SQRService.java
@@ -3,13 +3,15 @@ package SQRPackage;
 public class SQRService {
     public long countSquareSearch(long minBorder, long maxBorder) {
         int count = 0;
+        int minValue = 10;
         int maxValue = 99;
-        for (int minValue = 10; minValue <= maxValue; minValue++) {
+        for (int i = 10; i <= maxValue; i++) {
             if (minValue * minValue >= minBorder) {
                 if (minValue * minValue <= maxBorder) {
                     count = count + 1;
                 }
             }
+            minValue++;
         }
         return (count);
     }
